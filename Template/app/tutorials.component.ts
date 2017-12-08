21dfc656900477bce52c5e73ee826eb9cc348dc6
sravanoruganti <o.sravan.oruganti@gmail.com>
2017-12-08 15:59:12 +0530

    angular examples

--- Template/app/tutorials.component.ts
+++ Template/app/tutorials.component.ts
@@ -3,17 +3,16 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'my-tutorials',
     template: `<h2> {{main}}</h2>
-    
-    <input type="text" [(ngModel)]="fname">
-    <input type="text" [(ngModel)]="lname">
-    Full name:{{fname}} {{lname}}`
-
-
+    <p *ngIf= "showElement">showElement</p>
+    <div [ngSwitch]="color">
+        <p *ngSwitchWhen="'red'">red color is shown</p>
+        <p *ngSwitchWhen="'blue'">blue color is shown</p>
+        <p *ngSwitchDefault>Invalid color</p>
+    </div> `
 })
 export class TutorialsComponent {
-      public main = "hello hii tutorial";
-
-    public fname;
-      public lname;
+    public main = "hello hii tutorial";
+    public showElement = false;
+    public   color = 'white';
 
 }
