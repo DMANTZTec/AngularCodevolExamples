f3cff6603cf0c0ca8dde7b089f2a08659b7599a1
sravanoruganti <o.sravan.oruganti@gmail.com>
2017-12-08 12:30:24 +0530

    angular examples

--- Template/app/app.component.ts
+++ Template/app/app.component.ts
@@ -1,7 +1,12 @@
-import { Component } from '@angular/core';
-
+import { Component } from '@angular/core'
+import { TutorialsComponent } from './tutorials.component';
 @Component({
-  selector: 'my-app',
-  template: '<h1>Hello World</h1>'
+    selector: 'my-app',
+    template : `<h1> Hello World from Joatmon</h1>
+    
+    <my-tutorials></my-tutorials>`,
+
+    directives: [TutorialsComponent]
 })
-export class AppComponent { }
+
+export class AppComponent {}
