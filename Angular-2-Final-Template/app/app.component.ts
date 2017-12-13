e7a8678ba2aa8e2e9afb7442e246db976a8ef4f0
sravanoruganti <o.sravan.oruganti@gmail.com>
2017-12-13 15:49:28 +0530

    added files

--- Angular-2-Final-Template/app/app.component.ts
+++ Angular-2-Final-Template/app/app.component.ts
@@ -1,21 +1,37 @@
-import { Component } from '@angular/core';
-import {FormControl,FormGroup} from "@angular/forms";
+import {Component, OnInit} from '@angular/core';
+import {FormGroup,FormBuilder,Validators} from "@angular/forms";
 
 @Component({
   selector: 'my-app',
-  templateUrl:'app/app.component.html'
+  templateUrl:'app/app.component.html',
+    styles: [`input.ng-invalid {border-left: 5px solid red;}
+    input.ng-valid {border-left: 5px solid green;}
+    `]
 })
-export class AppComponent {
-userForm = new FormControl({
-    name : new FormControl('bradon'),
+export class AppComponent implements OnInit {
+  userForm : FormGroup;
+  constructor(private _formBuilder:FormBuilder){}
+  ngOnInit(){
+    this.userForm = this._formBuilder.group({
+        name:['Bradon', [Validators.required,Validators.minLength(4),Validators.maxLength(10)]],
+        email: [],
+        address : this._formBuilder.group({
+            street: [],
+            city:[],
+            postalcode:[null,[Validators.pattern('^[1-9][0-9]{4}$')]]
+        })
+    })
+  }
+/*userForm = new FormControl({
+    name : new FormControl('bradon',[Validators.required,Validators.minLength(4),Validators.maxLength(10)]),
     email : new FormControl(),
     address : new FormGroup({
         street : new FormControl(),
         city : new FormControl(),
-        postalcode : new FormControl()
+        postalcode : new FormControl(null, Validators.pattern('^[1-9][0-9]{4}$'))
 
     })
-});
+});*/
 onSubmit(){
   console.log(this.userForm.value);
 }
